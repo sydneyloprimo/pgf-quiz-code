b1350bcd34c1941bb78d6cc3f4aaa61e0e6e3b3c
Maximiliano Pawlowski <36646223+MaxiPawlowski@users.noreply.github.com>
2023-05-16 16:27:02 +0200

    [Bugfix]: Linting issue fix and build (#2)
    * fix linting issue
    
    * fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+
 import Providers from 'utils/provider'
 
 const inter = Inter({ subsets: ['latin'] })
--- shopify/generated/graphql.ts
+++ shopify/generated/graphql.ts
@@ -1,5 +1,4 @@
 import { GraphQLClient } from 'graphql-request';
-import { RequestInit } from 'graphql-request/dist/types.dom';
 import { useQuery, useInfiniteQuery, UseQueryOptions, UseInfiniteQueryOptions } from '@tanstack/react-query';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
