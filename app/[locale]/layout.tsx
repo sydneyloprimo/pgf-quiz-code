8819c3d984c0f580324abdf280e3f2db4a402a9a
Kevin <kevin.rodriguez@rootstrap.com>
2024-04-02 19:37:05 +0100

    SCS-169 - Added Clarity Analytics

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 
 import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
+import Script from 'next/script'
 import { useLocale } from 'next-intl'
 import { PropsWithChildren } from 'react'
 
@@ -35,6 +36,17 @@ export default function RootLayout({ children, params }: RootLayoutProps) {
 
   return (
     <html className="bg-dark-violet" lang={locale}>
+      <head>
+        <Script id="clarity-analytics">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "lqcfxk6bco");
+            `}
+        </Script>
+      </head>
       <body className="flex items-center flex-col">
         <BodyScripts />
         <Providers params={params}>
