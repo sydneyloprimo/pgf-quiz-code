48b148c0984eab07de74ca56f23f3c750199132f
Juan Andres <juanlacu@gmail.com>
2024-08-12 13:54:03 -0300

    update shopify version

--- shopify/client.ts
+++ shopify/client.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from 'graphql-request'
 
-const API_URL = `https://${process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN}/api/2023-01/graphql.json`
+const API_URL = `https://${process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN}/api/2024-10/graphql.json`
 
 const client = new GraphQLClient(API_URL, {
   headers: {
