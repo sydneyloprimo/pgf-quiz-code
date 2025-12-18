5bf07541be33b3e98984d3a4d02a43d77c1a8034
felipegarcia92 <felipegarcia92@gmail.com>
2025-12-18 15:53:32 -0300

    Split on different files for readability

--- components/home/HowItWorksSection/index.tsx
+++ components/home/HowItWorksSection/index.tsx
@@ -1,76 +1,9 @@
 import Image from 'next/image'
 import { useTranslations } from 'next-intl'
 
+import { StepCard } from '@/components/home/HowItWorksSection/StepCard'
 import { HOW_IT_WORKS_STEPS } from '@/constants'
 import { cn } from '@/utils/cn'
-
-interface StepCardProps {
-  stepNumber: number
-  title: string
-  description: string
-  isLast?: boolean
-  isFirst?: boolean
-}
-
-const StepCard = ({
-  stepNumber,
-  title,
-  description,
-  isLast = false,
-  isFirst = false,
-}: StepCardProps) => (
-  <div
-    className={cn(
-      'relative',
-      'flex flex-col md:flex-row items-start md:items-center',
-      'w-full py-11',
-      'border-b border-quaternary-500'
-    )}
-  >
-    {/* Vertical Divider Line (Desktop) */}
-    {!isFirst && (
-      <div
-        className={cn(
-          'absolute',
-          'top-0 left-16 h-8',
-          'w-px bg-quaternary-500'
-        )}
-        aria-hidden="true"
-      />
-    )}
-    {/* Step Number */}
-    <div className="w-32 shrink-0 flex items-center justify-center">
-      <span
-        className={cn(
-          'font-display',
-          'text-7xl md:text-8xl',
-          'leading-none tracking-tight',
-          'text-quaternary-200'
-        )}
-      >
-        {stepNumber}
-      </span>
-    </div>
-
-    {/* Content */}
-    <div className="flex-1 flex flex-col gap-4 pl-0 md:pl-8 pr-0 md:pr-16 pt-4 md:pt-0">
-      <h3
-        className={cn(
-          'font-display',
-          'text-2xl md:text-3xl',
-          'leading-tight md:leading-10',
-          'tracking-tight',
-          'text-neutral-white'
-        )}
-      >
-        {title}
-      </h3>
-      <p className={cn('font-sans text-lg leading-7', 'text-quaternary-100')}>
-        {description}
-      </p>
-    </div>
-  </div>
-)
 
 const HowItWorksSection = () => {
   const t = useTranslations('Home.HowItWorks')
