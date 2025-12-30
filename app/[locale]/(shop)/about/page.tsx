4dbb3c7635d91d117e994ea20e74fdc9b8616eaa
felipegarcia92 <felipegarcia92@gmail.com>
2025-12-30 18:25:35 -0300

    feat: Add Hero section to About page

--- app/[locale]/(shop)/about/page.tsx
+++ app/[locale]/(shop)/about/page.tsx
@@ -1,7 +1,9 @@
+import { HeroSection } from '@/components/about/HeroSection'
+
 export default function AboutPage() {
   return (
     <main className="flex flex-col items-center w-full bg-neutral-300">
-      {/* Sections will be added in subsequent branches */}
+      <HeroSection />
     </main>
   )
 }
