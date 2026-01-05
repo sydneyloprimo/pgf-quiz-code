0f4da79c00cb077c623c5fce50e5c084ffa8c43b
felipegarcia92 <felipegarcia92@gmail.com>
2026-01-05 12:34:13 -0300

    only display announcement toast in the homepage

---
+++ components/common/AnnouncementToast/HomepageWrapper.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+
+import { AnnouncementToast } from './index'
+
+import { Locale } from '@/i18n'
+import { Routes } from '@/types/enums/routes'
+
+
+const AnnouncementToastHomepageWrapper = () => {
+  const pathname = usePathname()
+  const isHomepage =
+    pathname === Routes.home ||
+    pathname === `/${Locale.EN}` ||
+    pathname === `/${Locale.ES}`
+
+  if (!isHomepage) {
+    return null
+  }
+
+  return <AnnouncementToast />
+}
+
+export { AnnouncementToastHomepageWrapper }
