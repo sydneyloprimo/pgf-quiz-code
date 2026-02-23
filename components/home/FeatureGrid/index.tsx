da3c581404853bb3c22355ca99cdb61fa62eee21
Lina <linafloriani8@gmail.com>
2026-02-23 15:16:22 -0300

    update icons for featureGrid

--- components/home/FeatureGrid/index.tsx
+++ components/home/FeatureGrid/index.tsx
@@ -2,11 +2,11 @@ import { useTranslations } from 'next-intl'
 
 import { FeatureGrid } from '@/components/common/FeatureGrid'
 import {
-  FeatureClinicIcon,
-  FeatureCommunityIcon,
-  FeatureEnvironmentIcon,
-  FeaturePharmIcon,
-  FeatureTransparentIcon,
+  SustainabilityIcon,
+  ScienceProof2Icon,
+  ScienceProofIcon,
+  ResearchDocumentIcon,
+  SmallDogIcon,
 } from '@/components/common/Icon'
 
 const HomeFeatureGrid = () => {
@@ -14,22 +14,22 @@ const HomeFeatureGrid = () => {
 
   const features = [
     {
-      icon: <FeatureClinicIcon className="size-16" />,
+      icon: <ScienceProofIcon className="size-16 text-secondary-950" />,
       title: t('feature1Title'),
       description: t('feature1Description'),
     },
     {
-      icon: <FeaturePharmIcon className="size-16" />,
+      icon: <ResearchDocumentIcon className="size-16 text-secondary-950" />,
       title: t('feature2Title'),
       description: t('feature2Description'),
     },
     {
-      icon: <FeatureTransparentIcon className="size-16" />,
+      icon: <ScienceProof2Icon className="size-16 text-secondary-950" />,
       title: t('feature3Title'),
       description: t('feature3Description'),
     },
     {
-      icon: <FeatureCommunityIcon className="size-16" />,
+      icon: <SmallDogIcon className="size-16 text-secondary-950" />,
       title: t('feature4Title'),
       description: t('feature4Description'),
     },
@@ -37,7 +37,7 @@ const HomeFeatureGrid = () => {
       type: 'special' as const,
     },
     {
-      icon: <FeatureEnvironmentIcon className="size-16" />,
+      icon: <SustainabilityIcon className="size-16 text-secondary-950" />,
       title: t('feature5Title'),
       description: t('feature5Description'),
     },
