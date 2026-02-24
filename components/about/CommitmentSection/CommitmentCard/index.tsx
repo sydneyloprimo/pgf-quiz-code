aac87b7eb729a61e4676182c725ee58b38252ca8
Lina <linafloriani8@gmail.com>
2026-02-24 12:55:43 -0300

    adjust text on mobile view

--- components/about/CommitmentSection/CommitmentCard/index.tsx
+++ components/about/CommitmentSection/CommitmentCard/index.tsx
@@ -10,12 +10,12 @@ interface CommitmentCardProps {
 const CommitmentCard = ({ text, hasBorder = false }: CommitmentCardProps) => (
   <div
     className={cn(
-      'flex flex-col lg:flex-row flex-1 gap-10 items-center lg:items-center lg:pl-5 pr-14 py-16 lg:py-5 min-w-0 w-full relative',
+      'px-5 flex flex-col lg:flex-row flex-1 gap-10 items-center lg:items-center lg:pl-5 lg:pr-14 py-16 lg:py-5 min-w-0 w-full relative',
       hasBorder && 'lg:border-r border-r-0 lg:border-r-quaternary-200'
     )}
   >
     <div className="flex gap-10 items-center lg:w-auto w-full justify-center">
-      <p className="font-sans font-normal text-base leading-6 text-quaternary-800 flex-1 text-left">
+      <p className="font-sans font-normal text-base leading-6 text-quaternary-800 flex-1 text-center lg:text-left">
         {text}
       </p>
     </div>
