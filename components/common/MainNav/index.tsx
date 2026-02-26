f00b169ca67f0b132cae0130411ae5279c1134bf
Lina <linafloriani8@gmail.com>
2026-02-26 12:11:50 -0300

    make nav transparent on all screen sizes

--- components/common/MainNav/index.tsx
+++ components/common/MainNav/index.tsx
@@ -111,7 +111,7 @@ const MainNav = () => {
       className={cn(
         'w-full group',
         isHome && !isScrolled
-          ? 'fixed top-0 left-0 right-0 z-50 bg-secondary-950 lg:bg-transparent'
+          ? 'fixed top-0 left-0 right-0 z-50 bg-transparent'
           : isHome && isScrolled
             ? 'fixed top-0 left-0 right-0 z-50 bg-secondary-950'
             : 'relative bg-secondary-950',
